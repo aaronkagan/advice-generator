35c81652708fa8c78eb54e54c83db0cd2f69339b
Aaron Kagan <aaronkagan@yahoo.com>
2023-11-17 10:44:39 -0500

    app functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from 'react';
+import die from './assets/images/icon-dice.svg';
 function App() {
-  return <></>;
+  const [count, setCount] = useState(0);
+  const [advice, setAdvice] = useState('');
+
+  const getAdvice = async () => {
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  };
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
+  return (
+    <main>
+      <p>Advice # {count}</p>
+      <p>{advice}</p>
+      <img
+        onClick={getAdvice}
+        role="button"
+        aria-label="Get Advice"
+        src={die}
+        alt="Dice Icon"
+      />
+    </main>
+  );
 }
 
 export default App;
